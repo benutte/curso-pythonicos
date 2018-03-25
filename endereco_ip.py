4fdf217d0814e05b9731c7d7795746996ac5cf1e
benutte <benutte20@gmail.com>
2018-03-25 10:17:09 -0300

    new file

---
+++ endereco_ip.py
@@ -0,0 +1,4 @@
+ip = raw_input("Digite um endereco IP: ")
+#print " O endereco " + ip + " e um endereco valido!"
+print " O endereco %s e um endereco valido!" % ip
+print " O endreco %s tem tamanho de %s caracteres" %(ip, str(len(ip)))
